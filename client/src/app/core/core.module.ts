e8875701ca80bb6e56225d1601a6eed375d96fba
AD\petar.korudzhiev <petar.korudzhiev@mentormate.com>
2017-12-18 13:39:39 +0200

    successfuly received data using redux

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Optional, SkipSelf, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @NgModule({
@@ -7,4 +7,17 @@ import { CommonModule } from '@angular/common';
   ],
   declarations: []
 })
-export class CoreModule { }
+
+export class CoreModule {
+  constructor( @Optional() @SkipSelf() parentModule: CoreModule) {
+    if (parentModule) {
+      throw new Error(
+        'CoreModule is already loaded. Import it in the AppModule only');
+    }
+  }
+  static forRoot(): ModuleWithProviders {
+    return {
+      ngModule: CoreModule
+    };
+  }
+}
--- client/src/app/products/products.module.ts
+++ client/src/app/products/products.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
 
 import { ProductsRoutingModule } from './products-routing.module';
 import { ProductsService } from './products.service';
 import { ProductsListComponent } from './products-list/products-list.component';
 import { ProductComponent } from './product/product.component';
 import { ProductDetailsComponent } from '../products/product-details/product-details.component';
+import { ProductsEffects } from './products-effects';
+import { products } from './products.reducer';
+
 
 @NgModule({
   imports: [
     CommonModule,
-    ProductsRoutingModule
+    ProductsRoutingModule,
+    StoreModule.forFeature('products', products),
+    EffectsModule.forFeature([ProductsEffects])    
   ],
   declarations: [ProductsListComponent, ProductComponent, ProductDetailsComponent],
   providers: [ProductsService]
