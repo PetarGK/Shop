e7ff239f0bf46db9abbe11396871544b67719a2a
AD\petar.korudzhiev <petar.korudzhiev@mentormate.com>
2018-01-11 13:10:19 +0200

    Made call from angular to AWS serverless product service

--- server/products/handlers.ts
+++ server/products/handlers.ts
@@ -31,6 +31,9 @@ module.exports.products = (event, context, callback) => {
 
         const response = {
           statusCode: 200,
+          headers: {
+            'Access-Control-Allow-Origin': '*'
+          },
           body: JSON.stringify(products)
         }
         console.log('before to return results - get products request')
